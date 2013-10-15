6ea4a860942a08a445582139ac7e624783353397
Khalid Abuhakmeh <khalid@aquabirdconsulting.com>
2013-10-15 19:07:07 -0400

    starting the faceted search sample.

---
+++ src/RavenDbTalk.Tests/6_Faceted_Search.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Raven.Tests.Helpers;
+using Xunit;
+
+namespace RavenDbTalk.Tests
+{
+    public class Faceted_Search : RavenTestBase
+    {
+        [Fact]
+        public void Can_perform_a_faceted_search()
+        {
+            // todo: create faceted
+        }
+    }
+}
